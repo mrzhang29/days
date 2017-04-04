1b1084e00d816e14e591fe1329c86655d7971368
mrzhang29 <mrzhang29@qq.com>
2017-04-05 01:33:07 +0800

    no message

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta http-equiv="Cache-Control" content="no-siteapp" />
 <meta http-equiv="X-UA-Compatible" content="IE=10,IE=9,IE=8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-<link href="favicon.ico" rel="icon" type="image/x-icon" />
+<link href="/favicon.ico" rel="icon" type="image/x-icon" />
 <?php
 if (git_get_option('git_robot_b')): ?>
 <?php
@@ -48,7 +48,6 @@ echo $sr_1
 echo $sr_2
 ?>]}
 </script>
-
 <!--[if lt IE 9]><script src="<?php
 echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script><![endif]-->
 <?php
@@ -58,10 +57,8 @@ echo get_post_meta($post->ID, 'git_customer', true); ?>
 if (git_get_option('git_customcss')) echo '<style type="text/css">'.git_get_option('git_customcss').'</style>'; ?>
 <body <?php
 body_class(); ?>>
-
- <?php/*
+<?php
 if (!git_get_option('git_pichead_b')) { ?>
-	
 <?php
 
     if (git_get_option('git_skin_b') == 'git_red_b') {
@@ -90,20 +87,18 @@ if (git_get_option('git_pichead_b')) { ?>
 <?php
     if (git_get_option('git_customhead')) { ?>
 <header style="background: url('<?php
-        echo git_get_option('git_customhead'); ?>')" id="header" class="header">
+        echo git_get_option('git_customhead'); ?>') center 0px repeat-x;background-size: cover;background-repeat:repeat-x\9" id="header" class="header">
 <?php
     } ?>
 <?php
     if (!git_get_option('git_customhead')) { ?>
 <header style="background: url('<?php
-        echo esc_url( get_template_directory_uri() ); ?>/css/img/header.jpg')" id="header" class="header">
+        echo esc_url( get_template_directory_uri() ); ?>/css/img/header.jpg') center 0px repeat-x;background-size: cover;" id="header" class="header">
 <?php
     } ?><?php
-} 
- */?>
-<header  id="header" class="header">
+} ?>
 <?php
-//if (git_get_option('git_tmnav_b')) echo '<style type="text/css">#nav-header{background:rgba(0,148,255,.5);background-color: rgba(0,148,255,.5);}</style>'; ?>
+if (git_get_option('git_tmnav_b')) echo '<style type="text/css">#nav-header{background-color: rgba(85,84,85, 0.5);background: rgba(85,84,85, 0.5);color: rgba(85,84,85, 0.5);}</style>'; ?>
 <?php
 if (git_get_option('git_skin_b') == 'git_red_b') {
 	$skin_nom = "#E74C3C";
@@ -130,12 +125,12 @@ if (git_get_option('git_skin_b') == 'git_red_b') {
 	$skin_nom = git_get_option('git_color_nom');
 	$skin_hover = git_get_option('git_color_hover');
 }
- echo '<style type="text/css">.navbar .nav li:hover a, .navbar .nav li.current-menu-item a, .navbar .nav li.current-menu-parent a, .navbar .nav li.current_page_item a, .navbar .nav li.current-post-ancestor a,.toggle-search ,#submit ,.btn,.pagination ul>.active>a,.pagination ul>.active>span,.bdcs-container .bdcs-search-form-submit,.metacat a{background: ' . $skin_nom . ';}.footer,.title h2,.shop-item .pricebtn{color: ' . $skin_nom . ';}.bdcs-container .bdcs-search-form-submit ,.bdcs-container .bdcs-search {border-color: ' . $skin_nom . ';}.pagination ul>li>a:hover,.navbar .nav li a:focus, .navbar .nav li a:hover,.toggle-search:hover,#submit:hover,.btn:hover,.pricebtn .buy {background-color: ' . $skin_hover . ';}.tooltip-inner{background-color:' . $skin_hover . ';}.tooltip.top .tooltip-arrow{border-top-color:' . $skin_hover . ';}.tooltip.right .tooltip-arrow{border-right-color:' . $skin_hover . ';}.tooltip.left .tooltip-arrow{border-left-color:' . $skin_hover . ';}.tooltip.bottom .tooltip-arrow{border-bottom-color:' . $skin_hover . ';}</style>';
+    echo '<style type="text/css">.navbar .nav li:hover a, .navbar .nav li.current-menu-item a, .navbar .nav li.current-menu-parent a, .navbar .nav li.current_page_item a, .navbar .nav li.current-post-ancestor a,.toggle-search ,#submit ,.btn,.pagination ul>.active>a,.pagination ul>.active>span,.bdcs-container .bdcs-search-form-submit,.metacat a{background: ' . $skin_nom . ';}.footer,.title h2,.shop-item .pricebtn{color: ' . $skin_nom . ';}.bdcs-container .bdcs-search-form-submit ,.bdcs-container .bdcs-search {border-color: ' . $skin_nom . ';}.pagination ul>li>a:hover,.navbar .nav li a:focus, .navbar .nav li a:hover,.toggle-search:hover,#submit:hover,.btn:hover,.pricebtn .buy {background-color: ' . $skin_hover . ';}.tooltip-inner{background-color:' . $skin_hover . ';}.tooltip.top .tooltip-arrow{border-top-color:' . $skin_hover . ';}.tooltip.right .tooltip-arrow{border-right-color:' . $skin_hover . ';}.tooltip.left .tooltip-arrow{border-left-color:' . $skin_hover . ';}.tooltip.bottom .tooltip-arrow{border-bottom-color:' . $skin_hover . ';}</style>';
 ?>
 <?php
-if (git_get_option('git_avataer_b')) echo '<style type="text/css">.avatar{-webkit-transition:0.4s;-webkit-transition:-webkit-transform 0.4s ease-out;transition:transform 0.4s ease-out;-moz-transition:-moz-transform 0.4s ease-out;}.avatar:hover{transform:rotateZ(360deg);-webkit-transform:rotateZ(360deg);-moz-transform:rotateZ(360deg);}</style>';?>
+if (git_get_option('git_avataer_b')) echo '<style type="text/css">.avatar{-webkit-transition:0.4s;-webkit-transition:-webkit-transform 0.4s ease-out;transition:transform 0.4s ease-out;-moz-transition:-moz-transform 0.4s ease-out;}.avatar:hover{transform:rotateZ(360deg);-webkit-transform:rotateZ(360deg);-moz-transform:rotateZ(360deg);}</style>'; ?>
 
-<!--<div class="container-inner">--><?php /*
+<div class="container-inner"><?php
 if (git_get_option('git_piclogo_left') && !G_is_mobile()) {
     echo '<div class="g-logo pull-left">';
 } else {
@@ -170,12 +165,8 @@ if (is_home()) {
     echo '</h1>';
 } else {
     echo '</div>';
-} */?>
-<!--</a></div></div>-->
-	<div id="toubuads">
-	
-
-	<?php
+} ?>
+</a></div></div><div id="toubuads"><?php
 if (git_get_option('git_toubuads') && git_get_option('git_piclogo_left') && !G_is_mobile()) echo git_get_option('git_toubuads'); ?></div>
 <?php
 if (git_get_option('git_skin_b') == 'git_red_b') {
@@ -193,40 +184,22 @@ if (git_get_option('git_skin_b') == 'git_red_b') {
 } elseif (git_get_option('git_skin_b') == 'git_green_b') {
     echo '<div id="nav-header" class="navbar" style="border-bottom: 4px solid #4CAF50 ;">';
 } elseif (git_get_option('git_skin_b') == 'git_custom_color') {
-   // echo '<div id="nav-header" class="navbar" style="border-bottom: 4px solid ' . git_get_option('git_color_nom') . ' ;">';
-   echo '<div id="nav-header" class="navbar">';
+    echo '<div id="nav-header" class="navbar" style="border-bottom: 4px solid ' . git_get_option('git_color_nom') . ' ;">';
 } else {
-  //  echo '<div id="nav-header" class="navbar" style="border-bottom: 4px solid #009966 ;">';
-   echo '<div id="nav-header" class="navbar">';
+    echo '<div id="nav-header" class="navbar" style="border-bottom: 4px solid #009966 ;">';
 } ?>
 
 <?php
-//if (git_get_option('git_bdshare_b')) echo '<style type="text/css"></style>'; 
-?>
-	
-	 <!--nav container-->
-	<!--<div class="nav-container">-->
-	<!--logo s-->
-	<div class="nav-logo">
-		<a href="/"><img title="<?php bloginfo('name'); ?>" alt="<?php bloginfo('name'); ?>" src="<?php echo git_get_option('git_customlogo'); ?>"></a>
-	</div>
-	<!--logo e-->
-	
-<ul class="nav site-nav site-navbar">
+if (git_get_option('git_bdshare_b')) echo '<style type="text/css">.bdsharebuttonbox a{cursor:pointer;border-bottom:0;margin-right:5px;width:28px;height:28px;line-height:28px;color:#fff}.bds_renren{background:#94b3eb}.bds_qzone{background:#fac33f}.bds_more{background:#40a57d}.bds_weixin{background:#7ad071}.bdsharebuttonbox a:hover{background-color:#7fb4ab;color:#fff;border-bottom:0}</style>'; ?>
+
+<ul class="nav">
 <?php
 echo str_replace('</ul></div>', '', preg_replace('/<div[^>]*><ul[^>]*>/', '', wp_nav_menu(array('theme_location' => 'nav', 'echo' => false))));?>
-<li style="float:right;">
-	<div class="toggle-search navto-search"><i class="fa fa-search"></i>
-	
-</div>
-<div class="search-expand" style="display: none;">
-	<div class="search-expand-inner">
+<li style="float:right;"><div class="toggle-search"><i class="fa fa-search"></i></div><div class="search-expand" style="display: none;"><div class="search-expand-inner">
     <?php
 if (git_get_option('git_search_baidu')) { ?>
     <?php
-    echo git_get_option('git_search_code'); ?>
-    </div>
-    </div>
+    echo git_get_option('git_search_code'); ?></div></div>
 <?php
 } elseif (git_get_option('git_search') && !git_get_option('git_search_baidu')) { ?>
 <form method="get" class="searchform themeform" onsubmit="location.href='/?s=' + encodeURIComponent(this.s.value).replace(/%20/g, '+'); return false;" action="/"><div><input type="ext" class="search" name="s" onblur="if(this.value=='')this.value='<?php
@@ -245,7 +218,6 @@ if (git_get_option('git_search_baidu')) { ?>
 </li>
 </ul>
 </div></div>
-<!--</div> --><!--nav container-->
 </header>
 <section class="container">
 <div class="speedbar">
